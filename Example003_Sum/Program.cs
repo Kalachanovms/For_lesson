9b623509f0fdf81ea4724bc8084f9bfe935051fb
Mikhail <Kalachanovs@gmail.com>
2022-11-22 22:33:53 +0300

    Example

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,7 @@
+﻿int NumberA = new Random().Next(1,10); // Случайное число 1 2 3 4 .... 9
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
+int result = NumberA + NumberB;
+Console.WriteLine(result);
+
