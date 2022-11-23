b1abb9b6a5b145ba4299c70278840e0e5cab58b8
Mikhail <Kalachanovs@gmail.com>
2022-11-23 07:45:32 +0300

    Task11

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray(int[] collectioin)
+{
+    int length = collectioin.Length;
+    int index = 0;
+    while (index < length)
+    {
+    collectioin[index]= new Random().Next(1, 10);
+        index ++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
